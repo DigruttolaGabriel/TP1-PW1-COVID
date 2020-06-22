599246d4a2659cbcebba766cfffb67c5a4bfc696
Julian Lopez <julyloopez01@gmail.com>
2020-06-22 19:16:08 -0300

    subo calendar

---
+++ js/calendario.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'dayGrid' ],
+      locale:'es',
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2019-08-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2019-08-07',
+          end: '2019-08-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2019-08-09T16:00:00'
+        },
+      ]
+
+    });
+
+    calendar.render();
+  });
