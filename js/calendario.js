34c9cca2baf4db57aad0bf4fe5c4c64314a7b3fb
digruttolagabriel <digruttolagabriel@gmail.com>
2020-06-22 22:00:32 -0300

    responsive

--- js/calendario.js
+++ js/calendario.js
@@ -2,26 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid' ],
-      locale:'es',
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2019-08-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2019-08-07',
-          end: '2019-08-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2019-08-09T16:00:00'
-        },
-      ]
-
+        plugins: ['dayGrid'],
+        locale: 'es',
+        events: [{
+                title: 'Primer vacunación\n(COVID-19)\nHospital Santojanni',
+                start: '2020-06-29T16:00:00',
+                end: '2020-06-29T17:00:00'
+            },
+            {
+                title: 'Segunda vacunación\n(COVID-19)\nHospital Posadas',
+                start: '2020-07-15',
+                end: '2020-07-15'
+            },
+            {
+                title: 'Tercer vacunación\n(COVID-19)\nHospital Zubizarreta',
+                start: '2020-08-16',
+                end: '2020-08-16'
+            },
+            {
+                title: 'Cuarta vacunación\n(COVID-19)\nHospital Zubizarreta',
+                start: '2020-08-17',
+                end: '2020-08-17'
+            },
+        ]
     });
-
     calendar.render();
-  });
+});
